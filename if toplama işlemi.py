0cf15d522cb63d9b9f521a76bed1f04b5218aa09
huerfanos <33223854+huerfanos@users.noreply.github.com>
2017-10-30 17:19:45 +0300

    Update if toplama işlemi.py

--- if toplama işlemi.py
+++ if toplama işlemi.py
@@ -3,6 +3,15 @@ s2=int(input("toplamak istediğiniz 2.sayı"))
 
 if s1 + s2 :
     print(s1+s2)
+    
+    
+   if kullanmadan yapmak isterseniz
+s1=int(input("toplamak istediğiniz 1.sayı"))
+s2=int(input("toplamak istediğiniz 2.sayı"))
+
+z=s1+s2
+print(z)
+ 
 
 
 
